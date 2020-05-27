2c5ee4ee1321901fdcf17c9677be52be2874f944
bitt4 <jakub.bittara@gmail.com>
2020-05-28 00:56:02 +0200

    Window width and height

--- include/consts.hpp
+++ include/consts.hpp
@@ -1,3 +1,6 @@
 #define CELL_WIDTH 16
-#define CELL_COUNT 24
-#define WINDOW_W CELL_COUNT*CELL_WIDTH+1
+#define CELLS_X 32	//number of cells in one row
+#define CELLS_Y 24	//number of cells in one column
+
+#define WINDOW_W CELLS_X*CELL_WIDTH
+#define WINDOW_H CELLS_Y*CELL_WIDTH
