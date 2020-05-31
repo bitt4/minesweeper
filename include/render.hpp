37d15037cc2289b6ab9912fa1badd508fe0ee857
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-05-31 23:06:26 +0200

    testing

--- include/render.hpp
+++ include/render.hpp
@@ -1,2 +1,4 @@
-void drawImage(SDL_Renderer *renderer, int x, int y, const char *filename, int id);
+void drawImage(SDL_Renderer *renderer, int x, int y, int id);
+void initRender(SDL_Renderer *renderer);
 void renderField(SDL_Renderer *renderer);
+// void revealField(SDL_Renderer *renderer); testing
