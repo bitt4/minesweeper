37e9c0c05298dbb8e99d2b95f6925af71d152070
bitt4 <jakub.bittara@gmail.com>
2020-06-01 13:07:45 +0200

    reveal field

--- include/render.hpp
+++ include/render.hpp
@@ -1,4 +1,4 @@
 void drawImage(SDL_Renderer *renderer, int x, int y, int id);
 void initRender(SDL_Renderer *renderer);
 void renderField(SDL_Renderer *renderer);
-// void revealField(SDL_Renderer *renderer); testing
+void revealField(SDL_Renderer *renderer, const board_t *board);
