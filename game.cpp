5a8163dad14948b658c1baeec891090d057def1f
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-06-01 13:04:37 +0200

    initialize board

--- game.cpp
+++ game.cpp
@@ -1,7 +1,14 @@
 #include <SDL2/SDL.h>
 
+#include "include/consts.hpp"
 #include "include/game.hpp"
 #include "include/render.hpp"
+
+void board_t::init(){
+    for(int i = 0; i < CELLS_X*CELLS_Y; i++){
+        field[i] = Mine;
+    }
+}
 
 void handleClick(int x, int y, int button, SDL_Renderer *renderer){
     //std::cout << x/CELL_WIDTH << " " << y/CELL_WIDTH << "" << "\n";
