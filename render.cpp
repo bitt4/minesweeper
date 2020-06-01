bad9a4e6f3bff2c11cfb4615c68e041c16705266
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-06-01 13:06:19 +0200

    coordinates system

--- render.cpp
+++ render.cpp
@@ -1,8 +1,8 @@
 #include <SDL2/SDL.h>
 
-#include "include/render.hpp"
 #include "include/consts.hpp"
-
+#include "include/game.hpp"
+#include "include/render.hpp"
 
 SDL_Surface *surface = SDL_LoadBMP("bitmaps/items.bmp");
 SDL_Texture *texture;
@@ -24,8 +24,8 @@ void drawImage(SDL_Renderer *renderer, int x, int y, int id){
     sourceRect.w = 16;
     sourceRect.h = 16;
 
-    destRect.x = x/CELL_WIDTH*CELL_WIDTH;
-    destRect.y = y/CELL_WIDTH*CELL_WIDTH;
+    destRect.x = x*CELL_WIDTH;
+    destRect.y = y*CELL_WIDTH;
     destRect.w = CELL_WIDTH;
     destRect.h = CELL_WIDTH;
 
@@ -33,18 +33,17 @@ void drawImage(SDL_Renderer *renderer, int x, int y, int id){
 }
 
 void renderField(SDL_Renderer *renderer){
-    for(int i = 0; i < WINDOW_H; i += CELL_WIDTH){
-        for(int j = 0; j < WINDOW_W; j += CELL_WIDTH){
+    for(int i = 0; i < CELLS_Y; i++){
+        for(int j = 0; j < CELLS_X; j++){
             drawImage(renderer, j, i, 0);
         }
     }
 }
-/* testing
-void revealField(SDL_Renderer *renderer){
-    for(int i = 0; i < WINDOW_H; i += CELL_WIDTH){
-        for(int j = 0; j < WINDOW_W; j += CELL_WIDTH){
-            drawImage(renderer, j, i, TriggeredMine);
+
+void revealField(SDL_Renderer *renderer, const board_t *board){
+    for(int i = 0; i < CELLS_Y; i++){
+        for(int j = 0; j < CELLS_X; j++){
+            drawImage(renderer, j, i, board->field[i * CELLS_X + j]);
         }
     }
 }
-*/
