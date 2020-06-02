16264201aff85249836db31b13f4a8aad57b0315
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-06-02 22:35:07 +0200

    reveal tile function

--- include/render.hpp
+++ include/render.hpp
@@ -2,3 +2,4 @@ void drawImage(SDL_Renderer *renderer, int x, int y, int id);
 void initRender(SDL_Renderer *renderer);
 void renderField(SDL_Renderer *renderer);
 void revealField(SDL_Renderer *renderer, const board_t *board);
+void clickOnTile(SDL_Renderer *renderer, const board_t *board, int x, int y);
