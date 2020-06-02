93fa807dda584e3a662608a5e81a860fec79a74e
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-06-02 22:32:08 +0200

    generating mines and revealing tiles

--- game.cpp
+++ game.cpp
@@ -1,30 +1,42 @@
 #include <SDL2/SDL.h>
+#include <ctime>
+#include <cstdlib>
 
 #include "include/consts.hpp"
 #include "include/game.hpp"
 #include "include/render.hpp"
 
 void board_t::init(){
+    
+    srand(time(NULL));
+    int randomNumber;
+    
     for(int i = 0; i < CELLS_X*CELLS_Y; i++){
-        field[i] = Mine;
+        randomNumber = rand();
+        if(randomNumber%5 == 0){
+            field[i] = Mine;
+        }
+        else{
+            field[i] = Clear;
+        }
     }
 }
 
-void handleClick(int x, int y, int button, SDL_Renderer *renderer){
+void handleClick(SDL_Renderer *renderer, const board_t *board, int x, int y, int button){
     //std::cout << x/CELL_WIDTH << " " << y/CELL_WIDTH << "" << "\n";
 
     int id;
 
     switch(button){
     case 1:
-        id = 15;
+        clickOnTile(renderer, board, x, y);
         break;
     case 3:
-        id = 1;
+        drawImage(renderer, x, y, Flag); //still for testing
         break;
     default:
         return;
     }
 
-    drawImage(renderer, x, y, id);
+    //drawImage(renderer, x, y, id);
 }
