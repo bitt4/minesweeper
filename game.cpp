b92dfab734734d1c9e4f9fc68d96f75ecc1fe2a2
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-06-05 23:25:53 +0200

    get number of mines around function

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <ctime>
 #include <cstdlib>
+#include <iostream>
 
 #include "include/consts.hpp"
 #include "include/game.hpp"
@@ -9,10 +10,10 @@
 bool flags[CELLS_X*CELLS_Y];
 
 void board_t::init(){
-    
+
     srand(time(NULL));
     int randomNumber;
-    
+
     for(int i = 0; i < CELLS_X*CELLS_Y; i++){
         randomNumber = rand();
         if(randomNumber%5 == 0){
@@ -35,6 +36,48 @@ bool flagTile(int x, int y){
     }
 }
 
+int getNearbyMines(const board_t *board, int x, int y){
+    int countMines = 0;
+
+    // 3 mines above target position
+    if(y - 1 >= 0){
+        for(int i = -1; i < 2; i++){
+            if(x+i >= 0 && x+i <= CELLS_X-1){
+                if(board->field[ (y-1) * CELLS_X + x+i  ] == Mine){
+                    countMines++;
+                }
+            }
+        }
+    }
+
+    // mine left to target position
+    if(x-1 >= 0){
+        if(board->field[ y * CELLS_X + x-1 ] == Mine){
+            countMines++;
+        }
+    }
+
+    // mine right to target position
+    if(x+1 <= CELLS_X-1){
+        if(board->field[ y * CELLS_X + x+1 ] == Mine){
+            countMines++;
+        }
+    }
+
+    // 3 mines under target position
+    if(y + 1 <= CELLS_Y-1){
+        for(int i = -1; i < 2; i++){
+            if(x+i >= 0 && x+i <= CELLS_X-1){
+                if(board->field[ (y+1) * CELLS_X + x+i  ] == Mine){
+                    countMines++;
+                }
+            }
+        }
+    }
+
+    return countMines;
+}
+
 void handleClick(SDL_Renderer *renderer, const board_t *board, int x, int y, int button){
     //std::cout << x/CELL_WIDTH << " " << y/CELL_WIDTH << "" << "\n";
 
@@ -43,6 +86,7 @@ void handleClick(SDL_Renderer *renderer, const board_t *board, int x, int y, int
     switch(button){
     case 1:
         clickOnTile(renderer, board, flags, x, y);
+        std::cout << getNearbyMines(board, x, y) << "\n";
         break;
     case 3:
         drawImage(renderer, x, y, Flag); //still for testing
