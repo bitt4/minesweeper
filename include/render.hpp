e3af3d32004b96aa53150baa5f19ae973bc0563b
bitt4 <jakub.bittara@gmail.com>
2020-06-13 16:59:12 +0200

    revealGroupEmpty and some testing functions

--- include/render.hpp
+++ include/render.hpp
@@ -4,4 +4,6 @@ void renderField(SDL_Renderer *renderer);
 void revealField(SDL_Renderer *renderer, const board_t *board);
 void clickOnTile(SDL_Renderer *renderer, const board_t *board, const bool *flags, int x, int y);
 void renderFlag(SDL_Renderer *renderer, const bool *flags, int x, int y);
-void revealNearby(SDL_Renderer *renderer, const board_t *board, int x, int y);
+int revealNearby(SDL_Renderer *renderer, const board_t *board, int x, int y);
+void revealGroupEmpty(SDL_Renderer *renderer, const board_t *board, int x, int y);
+void revealEmpty(SDL_Renderer *renderer, const board_t *board);
