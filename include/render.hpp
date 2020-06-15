7493da094872e1f1a62d462f9d0bca365d24b83d
bitt4 <jakub.bittara@gmail.com>
2020-06-15 23:50:00 +0200

    mutable board

--- include/render.hpp
+++ include/render.hpp
@@ -2,7 +2,7 @@ void drawImage(SDL_Renderer *renderer, int x, int y, int id);
 void initRender(SDL_Renderer *renderer);
 void renderField(SDL_Renderer *renderer);
 void revealField(SDL_Renderer *renderer, const board_t *board);
-void clickOnTile(SDL_Renderer *renderer, const board_t *board, const bool *flags, int x, int y);
+void clickOnTile(SDL_Renderer *renderer, board_t *board, bool *flags, int x, int y);
 void renderFlag(SDL_Renderer *renderer, const bool *flags, int x, int y);
 int revealNearby(SDL_Renderer *renderer, const board_t *board, int x, int y);
 void revealGroupEmpty(SDL_Renderer *renderer, const board_t *board, int x, int y);
