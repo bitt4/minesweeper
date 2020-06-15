3587580a2cbae76db80dcc1fc09c532cad4cc18e
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-06-15 23:48:20 +0200

    game state

--- include/game.hpp
+++ include/game.hpp
@@ -1,7 +1,8 @@
 typedef struct {
     int field[CELLS_X * CELLS_Y];
+    int state;
     void init();
 } board_t;
 
-void handleClick(SDL_Renderer *renderer, const board_t *board, int x, int y, int button);
+void handleClick(SDL_Renderer *renderer, board_t *board, int x, int y, int button);
 int getNearbyMines(const board_t *board, int x, int y);
