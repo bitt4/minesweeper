7cd2fddf024c9f24560ea20f4f26a33d8685a509
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-06-22 22:07:04 +0200

    removed useless comments and code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <SDL2/SDL.h>
 
 #include <iostream>
-//#include <cstdio>
 #include <cstdlib>
 
 //#define DEBUG
 
-//#define WINDOW_H 401
 #include "include/consts.hpp"
 #include "include/game.hpp"
 #include "include/render.hpp"
@@ -48,9 +46,6 @@ int main(int argc, char *argv[]){
     }
 
     std::cout << "Renderer creation successful\n";
-
-    //SDL_SetRenderDrawColor(renderer, 24, 24, 24, 255);
-    //SDL_RenderClear(renderer);
     
     initRender(renderer);
     renderField(renderer);
@@ -70,9 +65,7 @@ int main(int argc, char *argv[]){
                 quit = 1;
                 break;
             case SDL_MOUSEBUTTONDOWN:
-                //std::cout << "Mouse\n";
                 handleClick(renderer, &board, e.button.x/CELL_WIDTH, e.button.y/CELL_WIDTH, (int)e.button.button);
-                //std::cout << (int)e.button.button;
                 break;
 #ifdef DEBUG
             case SDL_KEYDOWN:
@@ -82,15 +75,6 @@ int main(int argc, char *argv[]){
             default: {}
             }
         }
-
-        //SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-        /*
-        for(int i = 0; i < WINDOW_W; i += CELL_WIDTH){
-            SDL_RenderDrawLine(renderer, i, 0, i, WINDOW_W);
-            SDL_RenderDrawLine(renderer, 0, i, WINDOW_W, i);
-        }
-        */
 
         SDL_RenderPresent(renderer);
     }
