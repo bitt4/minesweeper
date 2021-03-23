0bd6195e84b2ae626faac975eaf44c703389a00f
bitt4 <jakub.bittara@gmail.com>
2021-03-23 23:20:00 +0100

    Add some consts

--- include/Minesweeper.hpp
+++ include/Minesweeper.hpp
@@ -22,15 +22,15 @@ private:
     uint8_t* mines;
     SDL_Texture* cell_textures;
 
-    int get_width();
-    int get_height();
-    void draw_cell(SDL_Renderer* renderer, int x, int y);
-    int get_nearby_mines(int x, int y);
-    void set_flag(int x, int y, bool flagged);
-    void reveal_nearby_empty(int x, int y);
+    int get_width() const;
+    int get_height() const;
+    void draw_cell(SDL_Renderer* renderer, const int x, const int y);
+    int get_nearby_mines(const int x, const int y) const;
+    void set_flag(const int x, const int y, bool flagged);
+    void reveal_nearby_empty(const int x, const int y) const;
     void initialize_texture(SDL_Renderer* renderer);
 public:
-    Minesweeper(int width, int height, int difficulty);
+    Minesweeper(const int width, const int height, const int difficulty);
     ~Minesweeper();
     void render(SDL_Renderer *renderer);
     void mouse_down_event(SDL_Event *event);
