64f09f0c125645ab859ef2c39b23d4c38a7b2fd3
bitt4 <jakub.bittara@gmail.com>
2021-03-31 00:25:21 +0200

    Implement rendering

--- include/Minesweeper.hpp
+++ include/Minesweeper.hpp
@@ -13,16 +13,17 @@ private:
 
     std::vector<Cell> cells;
 
-    int get_width() const;
-    int get_height() const;
     int get_nearby_mines(const int x, const int y) const;
     void set_flag(const int x, const int y, bool flagged);
-    void initialize_texture(SDL_Renderer* renderer);
     void draw_cell(SDL_Renderer* renderer, const int x, const int y);
     void reveal_nearby_empty(SDL_Renderer* renderer, const int x, const int y);
 public:
     Minesweeper(const int width, const int height, const int difficulty = 7);
     ~Minesweeper();
+    int get_window_width() const;
+    int get_window_height() const;
+    void render_hidden_field(SDL_Renderer* renderer);
+    void initialize_texture(SDL_Renderer* renderer);
     void mouse_down_event(const SDL_Event& event);
 };
 
