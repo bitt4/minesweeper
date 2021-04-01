5b4d8160548afb4de7278dd61d0a1affd8426184
bitt4 <jakub.bittara@gmail.com>
2021-04-01 13:21:33 +0200

    Use renderer as a member of Minesweeper class

--- include/Minesweeper.hpp
+++ include/Minesweeper.hpp
@@ -10,20 +10,22 @@ private:
     const int m_width;
     const int m_height;
     const int m_difficulty;
+    SDL_Renderer* m_renderer;
 
     std::vector<Cell> cells;
 
     int get_nearby_mines(const int x, const int y) const;
     void set_flag(const int x, const int y, bool flagged);
-    void draw_cell(SDL_Renderer* renderer, const int x, const int y);
-    void reveal_nearby_empty(SDL_Renderer* renderer, const int x, const int y);
+    void draw_cell(const int x, const int y);
+    void reveal_nearby_empty(const int x, const int y);
 public:
     Minesweeper(const int width, const int height, const int difficulty = 7);
     ~Minesweeper();
     int get_window_width() const;
     int get_window_height() const;
-    void render_hidden_field(SDL_Renderer* renderer);
-    void initialize_texture(SDL_Renderer* renderer);
+    void assign_renderer(SDL_Renderer* renderer);
+    void render_hidden_field();
+    void initialize_texture();
     void mouse_down_event(const SDL_Event& event);
 };
 
