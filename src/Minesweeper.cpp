1ffec691b8749cf0eb43236ee246b08352cafaab
bitt4 <jakub.bittara@gmail.com>
2021-04-01 16:07:05 +0200

    Fix error in Minesweeper::get_nearby_mines()

--- src/Minesweeper.cpp
+++ src/Minesweeper.cpp
@@ -51,7 +51,7 @@ int Minesweeper::get_nearby_mines(const int x, const int y) const {
     int nearby = 0;
     for(int i = y - 1; i <= y + 1; ++i){
         for(int j = x - 1; j <= x + 1; ++j){
-            if(i != 0 && j != 0 && i >= 0 && j >= 0 && i < m_height && j < m_width){
+            if(!(i == y && j == x) && i >= 0 && j >= 0 && i < m_height && j < m_width){
                 if(cells[i * m_height + j].type() == Cell::Type::Mine)
                     nearby++;
             }
