2a109c72ae24457de96bae54cfc387fb42e12a41
bitt4 <64093718+bitt4@users.noreply.github.com>
2021-04-07 22:27:09 +0200

    Fix bug
    I got so used to setting width equal to height that I didn't notice
    that I used height instead of width in coordinates to index conversion.

--- src/Minesweeper.cpp
+++ src/Minesweeper.cpp
@@ -40,7 +40,7 @@ int Minesweeper::get_nearby_mines(const int x, const int y) const {
     for(int i = y - 1; i <= y + 1; ++i){
         for(int j = x - 1; j <= x + 1; ++j){
             if(!(i == y && j == x) && valid_coordinates(j, i)){
-                if(cells[i * m_height + j].type() == Cell::Type::Mine)
+                if(cells[i * m_width + j].type() == Cell::Type::Mine)
                     nearby++;
             }
         }
@@ -78,7 +78,7 @@ void Minesweeper::initialize_texture(){
 void Minesweeper::reveal_nearby_empty(const int x, const int y){
     for(int ry = y - 1; ry <= y + 1; ++ry){
         for(int rx = x - 1; rx <= x + 1; ++rx){
-            Cell &current_cell = cells[ry * m_height + rx];
+            Cell &current_cell = cells[ry * m_width + rx];
             if(!(ry == y && rx == x) && valid_coordinates(rx, ry)){
                 if(current_cell.type() != Cell::Type::Mine && !current_cell.revealed()){
                     current_cell.reveal();
@@ -123,7 +123,7 @@ void Minesweeper::generate_cells(){
     m_number_of_mines = 0;
     for(int y = 0; y < m_height; ++y){
         for(int x = 0; x < m_width; ++x){
-            int pos = y * m_height + x;
+            int pos = y * m_width + x;
             Cell::Type cell_type = Cell::Type::Nearby0;
             if(m_generator() % m_difficulty == 0){
                 cell_type = Cell::Type::Mine;
@@ -135,7 +135,7 @@ void Minesweeper::generate_cells(){
 
     for(int y = 0; y < m_height; ++y){
         for(int x = 0; x < m_width; ++x){
-            int pos = y * m_height + x;
+            int pos = y * m_width + x;
             if(cells[pos].type() != Cell::Type::Mine){
                 int nearby = get_nearby_mines(x, y);
                 cells[pos].set_type(nearby_mines_to_cell_type(nearby));
@@ -155,7 +155,7 @@ void Minesweeper::mouse_down_event(const SDL_Event& event){
     int x = event.button.x / Cell::width;
     int y = event.button.y / Cell::width;
 
-    Cell &current_cell = cells[y * m_height + x];
+    Cell &current_cell = cells[y * m_width + x];
 
     switch(event.button.button){
     case SDL_BUTTON_LEFT:
